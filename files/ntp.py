4a4772973c1fd4a0776d55ffd96b1264184b7b4c
Pierre GINDRAUD <pgindraud@gmail.com>
2017-11-01 16:46:07 +0100

    fix facts script

--- files/ntp.py
+++ files/ntp.py
@@ -11,15 +11,11 @@ content=dict()
 
 version_re = re.compile('^ntpd\s+(?P<version>(?P<major>[0-9]+)[^ ]+)$')
 try:
-    result = subprocess.Popen(['/usr/bin/env', 'ntpd', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    (stdout, stderr) = result.communicate()
-    output = stdout + stderr
-    for line in output.split():
-        match = version_re.search(line)
-        if match:
-            content['version_full'] = match.group('version')
-            content['version_major'] = match.group('major')
-            break
+    result = subprocess.check_output(['/usr/bin/env', 'ntpd', '--version'], universal_newlines=True)
+    match = version_re.search(result)
+    if match:
+        content['version_full'] = match.group('version')
+        content['version_major'] = match.group('major')
 except subprocess.CalledProcessError as e:
     content['error'] = str(e)
 
